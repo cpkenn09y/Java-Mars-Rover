00bffd9d44056561ea65f8095e66ba9a585165bc
Kenneth Yu <kenyu@ucdavis.edu>
2014-08-30 01:06:37 -0700

    wrote tests for Grid class
    * tests number of rows
    * tests number of columns
    * tests that grid body is a two dimensional array of integers with proper dimensions

---
+++ src/test/java/MarsRoverTests/GridTest.java
@@ -0,0 +1,46 @@
+package MarsRoverTests;
+
+import MarsRover.Grid;
+import org.junit.Test;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by KennethYu on 8/30/14.
+ */
+public class GridTest {
+    @Test
+    public void testCols() {
+        Grid testGrid = new Grid(3, 6);
+        assertEquals(testGrid.cols, 4);
+    }
+
+    @Test
+    public void testRows() {
+        Grid testGrid = new Grid(3, 6);
+        assertEquals(testGrid.rows, 7);
+    }
+
+    @Test
+    public void testBody2x2() {
+        Grid testGrid1 = new Grid(2,2);
+        int[][] intArrayTwoD = {{0,0,0}, {0,0,0}, {0,0,0}};
+        assertArrayEquals(
+            "A 2x2 grid has 3 columns and 3 rows, a total of 9 coordinates",
+            testGrid1.body,
+            intArrayTwoD
+        );
+    }
+
+    @Test
+    public void testBody2x3() {
+        Grid testGrid2 = new Grid(2,3);
+        int[][] intArrayTwoD = {{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}};
+        assertArrayEquals(
+                "A 2x3 grid has 3 columns and 4 rows, totaling 12 coordinates",
+                testGrid2.body,
+                intArrayTwoD
+        );
+    }
+
+}
